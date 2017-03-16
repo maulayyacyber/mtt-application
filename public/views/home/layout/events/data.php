7f733c79905d18fb64ad11541a4fe1e3edb2a7bf
maulayyacyber <maulayyacyber@gmail.com>
2017-03-16 09:17:31 +0700

    add form search events

--- public/views/home/layout/events/data.php
+++ public/views/home/layout/events/data.php
@@ -6,9 +6,24 @@
         </div>
 
         <div class="row news-v2">
-            <div id="events">
-
+            <div class="col-md-12" style="margin-bottom: 20px">
+                <div class="search-events" style="text-align: center">
+                    <form method="GET" action="<?php echo base_url('events/search');?>" style="margin-top: 10px">
+                        <div class = "input-group">
+                            <input type = "text" name = "q" class = "form-control input-lg" placeholder="Masukkan Nama Events dan Enter" autocomplete="off" id="articles">
+                            <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+                            <span class = "input-group-btn">
+                                  <button class = "btn btn-default btn-lg" type = "submit">
+                                     <i class="fa fa-search"></i> Search
+                                  </button>
+                               </span>
+                        </div>
+                    </form>
+                </div>
             </div>
+                <div id="events" style="margin-top: 20px">
+
+                </div>
         </div>
         <div class="row" style="text-align: center;margin-top: 20px">
             <button class="btn-u btn-u-sea" id="load_more_events" data-val = "0">More events <img style="display: none" id="loader" src="<?php echo base_url('resources/images/loader.svg') ?>" style="width: 5px;height: 5px"></button>
