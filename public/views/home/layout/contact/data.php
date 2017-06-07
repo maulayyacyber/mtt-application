c00ba6a2311b1c5ab18f018bebf5a2f0ed384d7e
Fika Ridaul Maulayya <ridaulmaulayya@gmail.com>
2017-06-07 12:56:15 +0700

    add iframe maps

--- public/views/home/layout/contact/data.php
+++ public/views/home/layout/contact/data.php
@@ -8,12 +8,10 @@
         <div class="row news-v2">
             <div class="col-md-12" style="margin-bottom: 20px">
                 <div class="page-detail" style="margin-bottom:10px;-moz-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);padding: 15px 15px;background-color: #fff">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254832.60117465255!2d98.52940472273195!3d3.6422756423793645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30313272ec60b6f9%3A0xe76e4e87481de374!2sKota+Medan%2C+Sumatera+Utara!5e0!3m2!1sid!2sid!4v1496814630092" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
             </div>
-                <div class="col-md-8">
-                    <div class="page-detail" style="margin-bottom:10px;-moz-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);padding: 15px 15px;background-color: #fff">
-                    </div>
-                </div>
+                
                 <div class="col-md-4">
                     <div class="page-detail" style="margin-bottom:10px;-moz-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);padding: 15px 15px;background-color: #fff;color: #000">
                         <i class="fa fa-map-marker"></i> <b>Alamat kantor</b> :
@@ -24,11 +22,14 @@
                         <?php echo systems('no_telp') ?><br>
                         <?php echo systems('no_telp2') ?>
                     </div>
+                </div>
+                <div class="col-md-4">
                     <div class="page-detail" style="margin-bottom:10px;-moz-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);padding: 15px 15px;background-color: #fff;color: #000">
                         <h3 class="text-center">Scan disini</h3>
                         <img src="<?php echo base_url() ?>resources/images/contact-us/pin-bbm.png" class="img-responsive">
                     </div>
-
+                </div>
+                <div class="col-md-4">
                      <div class="page-detail" style="margin-bottom:10px;-moz-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);padding: 15px 15px;background-color: #fff;color: #000">
                         <h3 class="text-center">Scan disini</h3>
                         <img src="<?php echo base_url() ?>resources/images/contact-us/kontak-us-line.png" class="img-responsive img-center">
